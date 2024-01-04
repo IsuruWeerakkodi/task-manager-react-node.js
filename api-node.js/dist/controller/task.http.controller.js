64e3a9d0333c7107898f36680310221c2383ba0f
Isuru Weerakkodi <isuruweerakkodi24@gmail.com>
2024-01-05 02:06:26 +0530

    add to and controller

---
+++ api-node.js/dist/controller/task.http.controller.js
@@ -0,0 +1,4 @@
+import { Router } from "express";
+const controller = Router();
+export { controller as TaskHttpController };
+//# sourceMappingURL=task.http.controller.js.map
---
+++ api-node.js/src/controller/task.http.controller.ts
@@ -0,0 +1,6 @@
+import {Router} from "express";
+
+const controller = Router();
+
+
+export {controller as TaskHttpController};
