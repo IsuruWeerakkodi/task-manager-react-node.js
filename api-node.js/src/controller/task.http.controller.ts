bf66fc1c75e2a20dc61b67e6a24a882d1caf4c5a
Isuru Weerakkodi <isuruweerakkodi24@gmail.com>
2024-01-05 02:28:42 +0530

    finish node.js backend

--- api-node.js/src/controller/task.http.controller.ts
+++ api-node.js/src/controller/task.http.controller.ts
@@ -1,6 +1,72 @@
-import {Router} from "express";
+import {Router} from 'express';
+import {Request, Response} from "express-serve-static-core";
+import mysql, {ResultSetHeader} from 'mysql2/promise';
+import {TaskTO} from "../to/task.to.js";
 
 const controller = Router();
 
-
+controller.get('/', getAllTasks);
+controller.post('/', saveTask);
+controller.patch('/:id', updateTask);
+controller.delete('/:id', deleteTask);
 export {controller as TaskHttpController};
+
+const pool = mysql.createPool({
+    database: 'dep11_todoapp',
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'mysql',
+    connectionLimit: 10
+})
+
+
+async function getAllTasks(req:Request, res:Response){
+    if (!req.query.email) res.sendStatus(400);
+    const connection = await pool.getConnection();
+    const [taskList] = await connection.execute('SELECT * FROM task WHERE email = ?', [req.query.email]);
+    res.json(taskList);
+    pool.releaseConnection (connection);
+}
+async function saveTask(req:Request, res:Response){
+    const  task = <TaskTO> req.body;
+    const connection = await pool.getConnection();
+    const [{insertId}] = await connection
+        .execute<ResultSetHeader>('INSERT INTO task (description, status, email) VALUES (?, false, ?)',
+            [task.description, task.email]);
+    pool.releaseConnection(connection);
+    task.id = insertId;
+    task.status = false;
+    res.status(201).json(task);
+}
+async function updateTask(req:Request, res:Response){
+    const taskId = +req.params.id;
+    const task = <TaskTO> req.body;
+    const connection = await pool.getConnection();
+    const result = await connection.execute('SELECT * FROM task WHERE id = ?', [taskId]);
+    if (result.length) {
+        res.sendStatus(404);
+        return;
+    }else{
+        await connection.execute('UPDATE task SET description = ? , status = ? WHERE id = ?', [
+            task.description, task.status, taskId
+        ]);
+        res.sendStatus(204);
+    }
+    pool.releaseConnection(connection);
+}
+async function deleteTask(req:Request, res:Response){
+    const taskId = +req.params.id;
+    const task = <TaskTO> req.body;
+    const connection = await pool.getConnection();
+    const result = await connection.execute('SELECT * FROM task WHERE id = ?', [taskId]);
+    if (result.length) {
+        res.sendStatus(404);
+        return;
+    }else{
+        await connection.execute('DELETE FROM task WHERE id = ?', [
+            taskId
+        ]);
+    }
+    pool.releaseConnection(connection);
+}
